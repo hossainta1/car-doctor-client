dec45c06e7e15828fe58054f1fc400b11d7fa182
hossainta1 <tanvir.hossain303030@gmail.com>
2023-11-14 19:04:17 +0600

    Create about us section

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -8,7 +8,7 @@ const Banner = () => {
         <div className="carousel w-full mt-1 h[600px]">
             <div id="slide1" className="carousel-item relative w-full">
                 <img src={img1} className="w-full rounded-xl" />
-                <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]">
+                <div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]">
                     <div className="text-white space-y-7 pl-12 w-1/2">
                         <h2 className="text-6xl font-bold">Affordable Price For Car Servicing</h2>
                         <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
@@ -25,6 +25,16 @@ const Banner = () => {
             </div>
             <div id="slide2" className="carousel-item relative w-full">
                 <img src={img2} className="w-full rounded-xl" />
+                <div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]">
+                    <div className="text-white space-y-7 pl-12 w-1/2">
+                        <h2 className="text-6xl font-bold">Affordable Price For Car Servicing</h2>
+                        <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                        <div>
+                            <button className="btn btn-primary mr-5">Discover More</button>
+                            <button className="btn btn-outline btn-secondary">Latest project</button>
+                        </div>
+                    </div>
+                </div>
                 <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
                     <a href="#slide1" className="btn btn-circle mr-5">❮</a>
                     <a href="#slide3" className="btn btn-circle">❯</a>
@@ -32,6 +42,16 @@ const Banner = () => {
             </div>
             <div id="slide3" className="carousel-item relative w-full">
                 <img src={img3} className="w-full rounded-xl" />
+                <div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]">
+                    <div className="text-white space-y-7 pl-12 w-1/2">
+                        <h2 className="text-6xl font-bold">Affordable Price For Car Servicing</h2>
+                        <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                        <div>
+                            <button className="btn btn-primary mr-5">Discover More</button>
+                            <button className="btn btn-outline btn-secondary">Latest project</button>
+                        </div>
+                    </div>
+                </div>
                 <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
                     <a href="#slide2" className="btn btn-circle mr-5">❮</a>
                     <a href="#slide4" className="btn btn-circle">❯</a>
@@ -39,6 +59,16 @@ const Banner = () => {
             </div>
             <div id="slide4" className="carousel-item relative w-full">
                 <img src={img4} className="w-full rounded-xl" />
+                <div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]">
+                    <div className="text-white space-y-7 pl-12 w-1/2">
+                        <h2 className="text-6xl font-bold">Affordable Price For Car Servicing</h2>
+                        <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                        <div>
+                            <button className="btn btn-primary mr-5">Discover More</button>
+                            <button className="btn btn-outline btn-secondary">Latest project</button>
+                        </div>
+                    </div>
+                </div>
                 <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
                     <a href="#slide3" className="btn btn-circle mr-5">❮</a>
                     <a href="#slide1" className="btn btn-circle">❯</a>
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,12 @@
 import Banner from "../Banner/Banner";
+import About from "./About/About";
 
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            <h2>This is Home</h2>
+            <About></About>
         </div>
     );
 };
