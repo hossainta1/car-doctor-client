da78e3b6d308a56ad1e2ae8ca3b71bcf0ba4a4d5
hossainta1 <tanvir.hossain303030@gmail.com>
2023-11-14 21:39:30 +0600

    Login and sign up in firebase ingregation

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,30 +1,28 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import img from '../../assets/images/login/login.svg'
-import { useContext } from 'react';
-import { AuthContext } from '../../providers/AuthProvider';
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
 
 
+const SignUp = () => {
 
-const Login = () => {
+    const { createUser } = useContext(AuthContext);
 
-    const { signIn } = useContext(AuthContext);
-
-    const handleLogin = event => {
+    const handleSignUp = event => {
         event.preventDefault();
         const form = event.target;
+        const name = form.name.value;
         const email = form.email.value;
         const password = form.password.value;
-        console.log(email, password);
+        console.log(name, email, password);
 
-        signIn(email, password)
+        createUser(email, password)
             .then(result => {
                 const user = result.user;
                 console.log(user);
             })
             .catch(error => console.log(error));
-
     };
-
     return (
         <div className="hero min-h-screen bg-base-200 mt-3">
             <div className="hero-content flex-col lg:flex-row">
@@ -33,9 +31,15 @@ const Login = () => {
                     <img src={img} alt="" />
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <form className="card-body" onSubmit={handleLogin}>
-                        <h1 className="text-3xl text-center font-bold">Login now!</h1>
+                    <form className="card-body" onSubmit={handleSignUp}>
+                        <h1 className="text-3xl text-center font-bold">Sign Up now!</h1>
 
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="name" name='name' placeholder="name" className="input input-bordered" required />
+                        </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
@@ -44,7 +48,7 @@ const Login = () => {
                         </div>
                         <div className="form-control">
                             <label className="label">
-                                <span className="label-text">Password</span>
+                                <span className="label-text">Confirm Password</span>
                             </label>
                             <input type="password" name='password' placeholder="password" className="input input-bordered" required />
                             <label className="label">
@@ -54,14 +58,15 @@ const Login = () => {
                         <div className="form-control mt-6">
 
 
-                            <input className='btn btn-primary' type="submit" value="Login" />
+                            <input className='btn btn-primary' type="submit" value="Sign Up" />
                         </div>
                     </form>
-                    <p className='my-4 text-center'>New to car doctor? <Link className='text-orange-500 font-bold' to='/signup'>Sign Up</Link> </p>
+                    <p className='my-4 text-center'>Alrady have an account? <Link className='text-orange-500 font-bold' to='/login'>Login</Link> </p>
+
                 </div>
             </div>
         </div>
     );
 };
 
-export default Login;
+export default SignUp;
